4b3a16d45730b64ff24c9ef3263fe58fea25f0b3
Jay Logue <jaylogue@google.com>
2019-08-29 19:55:56 -0700

    openweave-esp32-lwip-patch-0001 : Add custom core locking
    Added ability for the host environment to define locking functions that
    protect entry to the LwIP stack by the LwIP tcpip thread.  This enables
    coordinated use of the LwIP "raw" APIs from multiple threads.
    
    This feature differs from LwIP's experimental TCPIP_CORE_LOCKING feature
    in that it does not alter the behavior of other LwIP APIs such as the
    netconn API.  Note that the two features cannot be used together.

--- port/esp32/include/lwipopts.h
+++ port/esp32/include/lwipopts.h
@@ -29,6 +29,21 @@
  * Author: Simon Goldschmidt
  *
  */
+/*
+ * Copyright 2018 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 #ifndef __LWIPOPTS_H__
 #define __LWIPOPTS_H__
 
@@ -513,6 +528,12 @@
  * Don't use it if you're not an active lwIP project member
  */
 #define LWIP_TCPIP_CORE_LOCKING         0
+
+/**
+ * Enable custom core locking functions necessary for correct operation of Weave
+ * Inet Layer.
+ */
+#define LWIP_CUSTOM_CORE_LOCKING 1
 
 /*
    ------------------------------------
